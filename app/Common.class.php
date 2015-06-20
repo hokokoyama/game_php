085e1dfee4ab36ff1873bdd9d7f2aaf7ad7108eb
System Administrator <root@kissakisanyuu-no-MacBook-Air.local>
2015-06-20 10:49:05 +0900

    ログファイル書き込み処理

--- app/Common.class.php
+++ app/Common.class.php
@@ -2,10 +2,19 @@
 
 class Common
 {
-  public static function pp (){
-    $file_name = '../log/file.log';
+  public static function pp ($value){
+  	//日付取得
+  	$now = date('Ymd');
+  	//ファイル出力先
+    $file_name = "../log/".$now.".log";
+    //if(!file_exsits($file_name)){
+    	touch($file_name);
+    //}
 
-      touch($file_name);
+    //ファイル書き込み
+    $fp = fopen($file_name, 'w');
+    fwrite($fp, $value);
+    fclose($fp);
   }
 }
 ?>
--- app/top.php
+++ app/top.php
@@ -4,6 +4,10 @@ require 'Common.class.php';
 require '../smarty-3.1.23/libs/smarty.class.php';
 ini_set( 'display_errors', 1 );
 
+$test = 1;
+
+Common::pp($test);
+
 $smarty = new Smarty();
 
 $smarty->template_dir = dirname(__FILE__).'/templates';
