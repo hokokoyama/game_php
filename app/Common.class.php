965d7849d02838796111adfa68bf28ad1d13324a
System Administrator <root@kissakisanyuu-no-MacBook-Air.local>
2015-06-20 11:29:53 +0900

    ログに必要そうなもの表示

--- app/Common.class.php
+++ app/Common.class.php
@@ -2,18 +2,26 @@
 
 class Common
 {
+  //ログ出力
   public static function pp ($value){
   	//日付取得
   	$now = date('Ymd');
   	//ファイル出力先
     $file_name = "../log/".$now.".log";
-    //if(!file_exsits($file_name)){
+
+    //ファイルの存在有無
+    if(!file_exists($file_name)){
     	touch($file_name);
-    //}
+    }
+
+    //ログ用の時刻
+    $write_date = date('Y/m/d H:i:s');
+
+    $content = $write_date . '  ' . $value; 
 
     //ファイル書き込み
     $fp = fopen($file_name, 'w');
-    fwrite($fp, $value);
+    fwrite($fp, $content);
     fclose($fp);
   }
 }
