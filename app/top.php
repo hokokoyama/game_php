d0194184b8a86c2eaba3dcc1da1897e5be0ba77b
System Administrator <root@kissakisanyuu-no-MacBook-Air.local>
2015-07-04 19:16:47 +0900

    DB接続処理

--- app/top.php
+++ app/top.php
@@ -1,12 +1,21 @@
 <?php
 
 require 'Common.class.php';
-require '../smarty-3.1.23/libs/smarty.class.php';
+require '../smarty-3.1.23/libs/Smarty.class.php';
 ini_set( 'display_errors', 1 );
 
 $test = 1;
 
-Common::pp($test);
+
+
+//DB接続
+try{
+	$pdo = new PDO('mysql:host=localhost;dbname=hokoyama','root','');
+}catch(PDOException $e){
+	Common::pp($test);	
+}
+
+
 
 $smarty = new Smarty();
 
