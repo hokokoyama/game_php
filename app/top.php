91bd38671d4e5b79fae49f4c2a1fb3e5f87bf339
System Administrator <root@kissakisanyuu-no-MacBook-Air.local>
2015-07-12 13:20:41 +0900

    ログイン後画面作成

--- app/top.php
+++ app/top.php
@@ -1,12 +1,23 @@
 <?php
+class TopContrllor{
+
+	public function new_entry(){
+		require_once 'models/db/UserBasicData.class.php';		
+		$userBasicData = new UserBasicData();
+		$userBasicData->InsertUserData();
+	}	
+}
+
+?>
+
+<?php
 
 require 'Common.class.php';
 require '../smarty-3.1.23/libs/Smarty.class.php';
-ini_set( 'display_errors', 1 );
+//ini_set( 'display_errors', 1 );
 
 $test = 1;
-
-
+$html = 'top.htm';
 
 //DB接続
 try{
@@ -15,7 +26,16 @@ try{
 	Common::pp($test);	
 }
 
+//新規登録
+if($_GET['new']){
+	$top = new TopContrllor();
+	$top->new_entry();
+}
 
+//ログイン
+if($_POST['login']){
+	$html = 'top_login.htm';
+}
 
 $smarty = new Smarty();
 
@@ -23,6 +43,6 @@ $smarty->template_dir = dirname(__FILE__).'/templates';
 $smarty->compile_dir = dirname(__FILE__).'/templates_c';
 
 $smarty->assign('name','TOP');
-$smarty->display('top.htm');
+$smarty->display($html);
 
 ?>
